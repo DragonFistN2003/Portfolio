df955e7e1de1f727cf49b90e2b46b1bfd471e9d7
DragonFist <abawaneh580@gmail.com>
2025-02-28 22:04:01 +0300

    Update LinkedIn link styling on AboutPage

--- PTF/src/pages/aboutpage/AboutPage.jsx
+++ PTF/src/pages/aboutpage/AboutPage.jsx
@@ -4,6 +4,12 @@ function AboutPage() {
             <h1 className="text-4xl font-bold mb-6">About Page</h1>
             <p className="mb-4 text-lg text-center">I made this page to make you able to know who I am as a developer and I will show you my projects.</p>
             <p className="mb-8 text-lg text-center">Feel free to check my projects down below.</p>
+            <a 
+                href="https://www.linkedin.com/in/ahmad-al-hamad-b25089256/"
+                className="mb-8 text-lg text-center text-blue-600 hover:underline"
+            >
+                Linkedin
+            </a>
         </div>
     );
 }
